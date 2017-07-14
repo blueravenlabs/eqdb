cfc3397135ae249b4c3f33b42cacaff9c27f62f0
Blue Kosorok <blue@seriesdigital.com>
2017-07-14 02:36:47 -0700

    - updating project.

---
+++ raven.eqdb/raven.eqdb.web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using raven.eqdb.web.ViewModels.Home;
+
+namespace raven.eqdb.web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            HomeIndexViewModel vm = new HomeIndexViewModel();
+
+            return View(vm);
+        }
+    }
+}
---
+++ raven.eqdb/raven.eqdb.web/ViewModels/Home/HomeIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace raven.eqdb.web.ViewModels.Home
+{
+    public class HomeIndexViewModel
+    {
+        public HomeIndexViewModel()
+        {
+
+        }
+    }
+}
